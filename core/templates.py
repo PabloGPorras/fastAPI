745204eb597173115a5f887968823a2ca7607736
Pablo G Porras <pablogeorgeporras@gmail.com>
2025-01-22 14:20:32 -0700

    LTS

--- core/templates.py
+++ core/templates.py
@@ -1,7 +1,19 @@
+import os
+import sys
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
-# Jinja2 templates setup
-templates = Jinja2Templates(directory="templates")
+app = FastAPI()
 
-# Add `getattr` to the template environment
-templates.env.globals['getattr'] = getattr
+# Determine the base path
+if getattr(sys, "frozen", False):  # Check if running as a PyInstaller bundle
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Update paths for templates and static files
+template_path = os.path.join(base_path, "app/templates")
+
+# Mount static files and load templates
+templates = Jinja2Templates(directory=template_path)
